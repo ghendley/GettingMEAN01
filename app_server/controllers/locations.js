d45dc8aee48da7b4a15de5edb2b25cee53df5289
Grant Hendley <grant.hendley@gmail.com>
2020-02-26 21:32:34 -0600

    Up to 7.4 -- Location details from db

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -40,8 +40,9 @@ const renderHomepage = (req, res, responseBody) => {
 
 /* GET 'home' page */
 const homeList = (req, res) => {
+    const path = 'locations';
     const requestOptions = {
-        url: `${apiUri}/locations`,
+        url: `${apiUri}/${path}`,
         method: 'GET',
         json: {},
         qs: {
@@ -69,58 +70,64 @@ const homeList = (req, res) => {
     );
 };
 
+const renderDetailPage = (req, res, location) => {
+    res.render('location-info', {
+        title: location.name,
+        pageHeader: {
+            title: location.name
+        },
+        sidebar: {
+            context: 'is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
+            callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
+        },
+        mapApiKey: process.env.GOOGLE_MAPS_API_KEY,
+        location
+    });
+};
+
+const showError = (req, res, status) => {
+    let title,
+        content;
+
+    if (status === 404) {
+        title = '404, page not found';
+        content = 'Oh dear. Looks like you can\'t find this page. Sorry.';
+    } else {
+        title = `${status}, something's gone wrong`;
+        content = 'Something, somewhere, has gone just a little bit wrong.';
+    }
+
+    res.status(status);
+    res.render('generic-text', {
+        title,
+        content
+    });
+};
+
 /* GET 'Location info' page */
 const locationInfo = (req, res) => {
-    res.render('location-info',
-        {
-            title: 'Starcups',
-            pageHeader: {
-                title: 'Loc8r',
-            },
-            sidebar: {
-                context: 'is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
-                callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
-            },
-            location: {
-                name: 'Starcups',
-                address: '125 High Street, Reading, RG6 1PS',
-                rating: 3,
-                facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-                coords: {lat: 51.455041, lng: -0.9690884},
-                openingTimes: [
-                    {
-                        days: 'Monday - Friday',
-                        opening: '7:00am',
-                        closing: '7:00pm',
-                        closed: false
-                    },
-                    {
-                        days: 'Saturday',
-                        opening: '8:00am',
-                        closing: '5:00pm',
-                        closed: false
-                    },
-                    {
-                        days: 'Sunday',
-                        closed: true
-                    }
-                ],
-                reviews: [
-                    {
-                        author: 'Simon Holmes',
-                        rating: 5,
-                        timestamp: '16 July 2013',
-                        reviewText: 'What a great place. I can\'t say enough good things about it.'
-                    },
-                    {
-                        author: 'Charlie Chaplin',
-                        rating: 3,
-                        timestamp: '16 June 2013',
-                        reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'
-                    }
-                ]
-            },
-            mapApiKey: process.env.GOOGLE_MAPS_API_KEY
+    const path = `locations/${req.params.locationid}`;
+    const requestOptions = {
+        url: `${process.env.API_URI}/${path}`,
+        method: 'GET',
+        json: {}
+    };
+
+    request(
+        requestOptions,
+        (err, {statusCode}, body) => {
+            const data = body;
+
+            if (statusCode === 200) {
+                data.coords = {
+                    lng: body.coordinates[0],
+                    lat: body.coordinates[1]
+                };
+
+                renderDetailPage(req, res, data);
+            } else {
+                showError(req, res, statusCode);
+            }
         }
     );
 };
