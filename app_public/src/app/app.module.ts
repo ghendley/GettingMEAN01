937656810162f29f2b5ff2d808ccbc2dd92e1650
Grant Hendley <grant.hendley@gmail.com>
2020-03-11 23:18:27 -0500

    Angular content with styling -- up to 8.2.3 in the book

--- app_public/src/app/app.module.ts
+++ app_public/src/app/app.module.ts
@@ -1,17 +1,17 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
-import {AppComponent} from './app.component';
+import {HomeListComponent} from './home-list/home-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    HomeListComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [HomeListComponent]
 })
 export class AppModule {
 }
