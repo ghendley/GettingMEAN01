cfa7e44a4860c93b518c52ce1b8d3c61667750a5
Grant Hendley <grant.hendley@gmail.com>
2020-03-26 23:21:10 -0500

    Through 9.2.2; angular routing etc

--- app_public/src/app/app.module.ts
+++ app_public/src/app/app.module.ts
@@ -2,21 +2,38 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {HttpClientModule} from "@angular/common/http";
+import {RouterModule} from "@angular/router";
 
 import {HomeListComponent} from './home-list/home-list.component';
 import {DistancePipe} from './distance.pipe';
+import {FrameworkComponent} from './framework/framework.component';
+import {AboutComponent} from './about/about.component';
+import {HomepageComponent} from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
     HomeListComponent,
-    DistancePipe
+    DistancePipe,
+    FrameworkComponent,
+    AboutComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomepageComponent
+      },
+      {
+        path: 'about',
+        component: AboutComponent
+      }
+    ])
   ],
   providers: [],
-  bootstrap: [HomeListComponent]
+  bootstrap: [FrameworkComponent]
 })
 export class AppModule {
 }
