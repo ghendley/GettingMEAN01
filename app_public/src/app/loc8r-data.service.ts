3b1c5edef7373ca4c5ed766a15ec556aa852a984
Grant Hendley <grant.hendley@gmail.com>
2020-03-26 23:38:51 -0500

    Dynamically handle api url via environment variables

--- app_public/src/app/loc8r-data.service.ts
+++ app_public/src/app/loc8r-data.service.ts
@@ -1,7 +1,8 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 
 import {Location} from './home-list/home-list.component';
+import {environment} from "../environments/environment";
 
 @Injectable({
   providedIn: 'root'
@@ -11,22 +12,23 @@ export class Loc8rDataService {
   constructor(private http: HttpClient) {
   }
 
+  private static handleError(error: any): Promise<any> {
+    console.error('Something has gone wrong', error);
+    return Promise.reject(error.message || error);
+  }
+
   public getLocations(): Promise<Location[]> {
     const lng: number = -96.29157257080078;
     const lat: number = 30.647083282470703;
     const maxDistance: number = 20000;
-    // TODO DO NOT HARD CODE URL HERE
-    const url: string = `http://localhost:3000/api/locations?lng=${lng}&lat=${lat}&maxkm=${maxDistance}`;
+    const apiUrl: string = environment["API_URI"];
+
+    const url: string = `${apiUrl}/locations?lng=${lng}&lat=${lat}&maxkm=${maxDistance}`;
 
     return this.http
       .get(url)
       .toPromise()
       .then(response => response as Location[])
       .catch(Loc8rDataService.handleError);
-  }
-
-  private static handleError(error: any): Promise<any> {
-    console.error('Something has gone wrong', error);
-    return Promise.reject(error.message || error);
   }
 }
